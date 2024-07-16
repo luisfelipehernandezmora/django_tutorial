a7faaa379636dd4409199e656265c5be32cd060c
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2024-07-16 00:03:30 -0600

    Solved partially part 3 of the tutorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 def index(request):
-    return(HttpResponse("Hello, world. You're at the polls index"))
+    latest_question_list=Question.objects.order_by("-pub_date")[:5]
+    # output=", ".join([q.question_text for q in latest_question_list])
+    # return(HttpResponse("Hello, world. You're at the polls index"))
+    # template=loader.get_template("polls/index.html")
+    context={
+        "latest_question_list":latest_question_list,
+    }
+    # return(HttpResponse(template.render(context,request)))
+    return render(request,"polls/index.html",context)
 
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at question {question_id}")
+
+def results(request, question_id):
+    response="You are looking at the results of question"
+    return HttpResponse(f"{response} {question_id}")
+
+def vote(request,question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
