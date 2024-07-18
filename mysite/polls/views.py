3d3e4002ff58156a527e57e30f41369ac63ed107
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2024-07-17 22:49:57 -0600

    part 4 tutorial, unfinished

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -24,6 +24,22 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model=Question
     template_name="polls/results.html"
+    
+def vote(request,question_id):
+    # return HttpResponse(f"You're voting on question {question_id}")
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        context={"question":question,"error_message":"You didn't select a choice."}
+        return render(request,"polls/detail.html",context)
+    else:
+        selected_choice.votes=F("votes") + 1
+        selected_choice.save()
+        #TIP!!!!!!: Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button. (...from django tutorial)
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 # def index(request):
 #     latest_question_list=Question.objects.order_by("-pub_date")[:5]
@@ -54,18 +70,3 @@ class ResultsView(generic.DetailView):
 #     question=get_object_or_404(Question,pk=question_id)
 #     return render(request,"polls/results.html",{"question":question})
 
-def vote(request,question_id):
-    # return HttpResponse(f"You're voting on question {question_id}")
-    question=get_object_or_404(Question,pk=question_id)
-    try:
-        selected_choice=question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        context={"question":question,"error_message":"You didn't select a choice."}
-        return render(request,"polls/detail.html",context)
-    else:
-        selected_choice.votes=F("votes") + 1
-        selected_choice.save()
-        #TIP!!!!!!: Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button. (...from django tutorial)
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
