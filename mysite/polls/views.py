cd6917da279a01ae16be7fd2d44f6ced53ee9fee
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2024-07-26 15:23:41 -0600

    solved test cases for part 5 of django official tututorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -32,14 +32,16 @@ class IndexView(generic.ListView):
         ### Django tutorial proposal of solution
         """"Return the last five published questions (not including those set to be published in the future)"""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("pub_date")[:5]
-        
-        
-        
     
 class DetailView(generic.DetailView):
     model=Question
     template_name="polls/detail.html"
     
+    def get_queryset(self):
+        """Excludes any quesitons that aren't published yet"""
+        return Question.objects.filter(pub_date__lte=timezone.now())
+        
+        
 class ResultsView(generic.DetailView):
     model=Question
     template_name="polls/results.html"
